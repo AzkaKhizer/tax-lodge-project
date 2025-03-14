7b9319eb9e9d5e2cc429fc092a71237de1806942
AzkaKhizer <144557215+AzkaKhizer@users.noreply.github.com>
2025-03-14 23:59:46 +0500

    Update page.tsx

--- src/app/Calculator/page.tsx
+++ src/app/Calculator/page.tsx
@@ -1,18 +1,1 @@
-import PayCalculator from "@/app/components/PayCalculator";
-import BreadcrumbCalc from "../components/BreadcrumbCalc";
-import WelcomeSection from "../components/WelcomeCalc";
-import TaxReturnSection from "../components/GetStarted";
 
-export default function Calculator() {
-  return (
-    <main className=" pt-28  mx-auto overflow-x-hidden">
-        <div>
-        <BreadcrumbCalc/>
-        <WelcomeSection/>
-        
-      <PayCalculator />
-      <TaxReturnSection/>
-      </div>
-    </main>
-  );
-}
