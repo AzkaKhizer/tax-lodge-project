81b467b099d58ac5c6c18a61c4b66741e00947af
AzkaKhizer <144557215+AzkaKhizer@users.noreply.github.com>
2025-03-15 00:01:32 +0500

    Update page.tsx

--- src/app/ServiceDetails/page.tsx
+++ src/app/ServiceDetails/page.tsx
@@ -1,14 +1,1 @@
-import BreadcrumbService from "../components/BCService";
-import TaxServices from "../components/Service";
-import HeroSection from "../components/TaxSolution";
 
-export default function ServiceDetails() {
-    return (
-      <div className=" mt-28 mx-auto overflow-x-hidden">
-        
-       <BreadcrumbService/>
-       <TaxServices/>
-       <HeroSection/>
-      </div>
-    );
-  }
