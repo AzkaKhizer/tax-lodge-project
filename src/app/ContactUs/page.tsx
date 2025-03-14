307e8034dafd98aac0f2fba13026948946d6f7ba
AzkaKhizer <144557215+AzkaKhizer@users.noreply.github.com>
2025-03-15 00:00:12 +0500

    Update page.tsx

--- src/app/ContactUs/page.tsx
+++ src/app/ContactUs/page.tsx
@@ -1,19 +1,1 @@
-import BreadcrumbContact from "../components/BCContact";
-import ContactForm from "@/app/components/ContactForm";
-import ContactInfo from "../components/ContactInfo";
-import HeroSection from "../components/TaxSolution";
 
-import React from 'react'
-
-const ContactUs = () => {
-  return (
-    <div className="mt-28  mx-auto overflow-x-hidden">
-        <BreadcrumbContact/>
-        <ContactForm/>
-        <ContactInfo/>
-        <HeroSection/>
-    </div>
-  )
-}
-
-export default ContactUs
